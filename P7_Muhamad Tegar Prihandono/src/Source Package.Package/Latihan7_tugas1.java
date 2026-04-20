3d59f4d68637dfac7bd97c7630d19d50de67843a
Crowsyollo <thohardjati@gmail.com>
2026-04-20 16:42:41 +0700

    Add files via upload

---
+++ P7_Muhamad Tegar Prihandono/src/Source Package.Package/Latihan7_tugas1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Latihan7_tugas1{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        float kehadiran;
+        float nilaiAkhir;
+        float persentase;
+        persentase = 21 * 75 / 100;
+
+        System.out.print("masukkan jumlah kehadiran = ");
+        kehadiran = input.nextFloat();
+        System.out.print("masukkan jumlah nilai akhir = ");
+        nilaiAkhir = input.nextFloat();
+
+        if (kehadiran >= persentase) {
+            if (nilaiAkhir >= 80) {
+                System.out.println("nilai = " + nilaiAkhir + " Grade = A");
+            } else if (nilaiAkhir >= 70) {
+                System.out.println("nilai = " + nilaiAkhir + " Grade = B");
+            } else if (nilaiAkhir >= 60) {
+                System.out.println("nilai =" + nilaiAkhir + " Grade = C");
+            } else if (nilaiAkhir >= 50) {
+                System.out.println("nilai = " + nilaiAkhir + " Grade = D");
+            } else if (nilaiAkhir < 40) {
+                System.out.println("nilai = " + nilaiAkhir + " Grade = E");
+            }
+        } else {
+            System.out.print("");
+        }
+    }
+}
